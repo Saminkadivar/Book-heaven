1ec3cc11cd92f3e4c1301ad7b3f11b0614d547f1
Saminkadivar <saminkadivar2911@gmail.com>
2025-06-11 15:00:53 +0530

    Update connection.php

--- public/connection.php
+++ public/connection.php
@@ -1,14 +1,1 @@
-<?php
-  session_start();
-  $con = mysqli_connect("localhost", "root", "", "mini_project");
-  if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-  }
 
-  
-  
-  define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
-  const SITE_PATH = 'localhost//BscitMiniProject3rdSem/';
-  
-  
-  ?>
